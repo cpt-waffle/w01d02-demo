c1470928b10fcddec5ecc10d8ae7078d430d34cb
vasiliy-klimkin <vasily.klimkin@gmail.com>
2021-11-23 11:21:06 -0500

    add a few more files

--- demo.js
+++ demo.js
@@ -1,2 +1,6 @@
+// git init                    --- initialized the repo
+// git add file_name           --- add to staging
+// git commit -m "__message__" --- stage the file(s) with the proper commit message!
+
 console.log("Hello World!");
 console.log("another hello world!");
