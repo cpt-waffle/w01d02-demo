8b46a1055a3df1bd15d4f87958ac8fad6d7c33ec
vasiliy-klimkin <vasily.klimkin@gmail.com>
2021-11-23 12:12:43 -0500

    refactor

--- realDemo/demo.js
+++ realDemo/demo.js
@@ -4,25 +4,11 @@
 // in and returns a sum of all the numbers.
 
 
-/////////////
-// How on earth am i able to add numbers?
-console.log(4 + 4);
-// how do i have multiple numbers ?
-let num1 = 1;
-let num2 = 2;
-let num3 = 3;
-const arr = [10,10,20,30,40]
-console.log(arr);
-// how do i go through EVERY SINGLE NUMBER in the array?
-//  use a for loop!!
+const arr = process.argv.slice(2);
 let sum = 0;
+console.log(arr);
 for (let i = 0; i <= arr.length - 1; i++) {
     sum = sum + Number(arr[i]);
 }
 
 console.log("The sum is: ", sum);
-
-
-// what are these "command line arguments"?
-// how do i utilize this?
-// how do i display all of this out?
