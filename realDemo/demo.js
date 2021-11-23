a6efb1b69fdb3f9f257d66247e61a878adf84be3
vasiliy-klimkin <vasily.klimkin@gmail.com>
2021-11-23 11:59:40 -0500

    work in progress

--- realDemo/demo.js
+++ realDemo/demo.js
@@ -1,1 +1,25 @@
-console.log("test");
+// We are tasked to create a program that takes 
+// in any number of command line arguments
+// which will be numbers, and adds them toghether 
+// in and returns a sum of all the numbers.
+
+
+/////////////
+// How on earth am i able to add numbers?
+console.log(4 + 4);
+// how do i have multiple numbers ?
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+const arr = [1,2,3,4,5,6,7,8];
+console.log(arr);
+// how do i go through EVERY SINGLE NUMBER in the array?
+//  use a for loop!!
+for (let i = 0; i <= arr.length - 1; i++) {
+    console.log("i is ===", i, '   arr[i] is ===',arr[i]);
+}
+
+
+// what are these "command line arguments"?
+// how do i utilize this?
+// how do i display all of this out?
